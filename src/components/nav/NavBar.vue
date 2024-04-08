8eec1bed7448315930913c9d4968f42f16c6793f
Aisuruneko <officialtcgmatt@gmail.com>
2024-04-08 04:52:13 -0400

    nav design should be like done; pls tweak

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -1,17 +1,24 @@
 <script setup>
+import { ref } from 'vue';
 import WikiLogo from '@/components/WikiLogo.vue';
 import navBarButton from './NavBarButton.vue';
 import { PhMagnifyingGlass } from "@phosphor-icons/vue";
+
+const isOpen = ref(false);
+
+const togNav = () => {
+	isOpen.value = !isOpen.value;
+};
 
 </script>
 
 
 <template>
     <nav>
-        <div class="navbar">
+		<div class="navbar">
 			<div id="daLeft">
-				<navBarButton id="menu"></navBarButton>
-            	<WikiLogo size="small" />
+				<navBarButton @click="togNav" id="menu"></navBarButton>
+				<WikiLogo size="small" />
 			</div>
 			<div id="daRight">
 				<div id="searchBarWrapper">
@@ -23,8 +30,28 @@ import { PhMagnifyingGlass } from "@phosphor-icons/vue";
 					<navBarButton id="discord"></navBarButton>
 				</div>
 			</div>
-        </div>
+		</div>
     </nav>
+	<Transition name="slidedown">
+    <nav v-if="isOpen" class="content">
+		<div class="navbar">
+			<div id="daLeft">
+				<navBarButton id="accountData"></navBarButton>
+				<router-link to="#" exact>Main Page</router-link>
+				<router-link to="#" exact>Staff Guidelines</router-link>
+				<router-link to="#" exact>How to Contribute</router-link>
+				<router-link to="#" exact>About Page</router-link>
+			</div>
+			<div id="daRight">
+				<router-link to="#" exact>Wiki Editor</router-link>
+				<router-link to="#" exact>Donate</router-link>
+			</div>
+		</div>
+		<Transition name="fade" appear>
+			<div v-if="isOpen" id="pageBlocker"></div>
+		</Transition>
+    </nav>
+	</Transition>
 </template>
 
 <style lang="scss">
@@ -34,13 +61,12 @@ nav {
     z-index: 800;
     top: 0;
     left: 0;
-
-    background-color: var(--background-3);
+	background-color: var(--background-3);
 
     .navbar {
-        width: var(--content-width);
-        height: var(--nav-height);
-        margin: 0 auto;
+		width: var(--content-width);
+		height: var(--nav-height);
+		margin: 0 auto;
 		display: flex;
 		justify-content: space-evenly;
 		#daLeft,
@@ -79,6 +105,75 @@ nav {
 				gap: 4px;
 			}
 		}
-    }
+	}
+
+	&.content {
+		top: var(--nav-height);
+		background-color: var(--background-1);
+
+		#pageBlocker {
+			z-index: -1;
+			position: fixed;
+			top: var(--nav-height);
+			left: 0;
+			width: 100%;
+			height: 100%;
+			background-color: var(--background-1);
+			opacity: .5;
+		}
+
+		.navbar {
+			#daLeft {
+				width: 100%;
+				gap: 16px;
+			}
+			#daRight {
+				width: 25%;
+			}
+			.router-link-active {
+				transition: color 0.2s ease-in-out;
+			}
+			.navbar .router-link-active:hover {
+				color: #F876D3;
+			}
+		}
+	}
 }
+
+.slidedown-enter-active,
+.slidedown-leave-active {
+  transition: max-height 0.2s ease-in-out;
+}
+
+.slidedown-enter-to,
+.slidedown-leave-from {
+  overflow: hidden;
+  max-height: var(--nav-height);
+}
+
+.slidedown-enter-from,
+.slidedown-leave-to {
+  overflow: hidden;
+  max-height: 0;
+}
+
+.fade-enter-active {
+	animation: fade-thing .25s;
+}
+.fade-leave-active {
+	animation: fade-thing .25s reverse;
+}
+
+@keyframes fade-thing {
+	0% {
+		opacity: 0;
+	}
+	50% {
+		opacity: 0;
+	}
+	100% {
+		opacity: .5;
+	}
+}
+
 </style>
--- src/components/nav/NavBarButton.vue
+++ src/components/nav/NavBarButton.vue
@@ -10,7 +10,7 @@ const props = defineProps({
 </script>
 
 <template>
-	<a v-if="id === 'menu'" href="#" class="navBarButton" :id>
+	<a v-if="id === 'menu'" class="navBarButton" :id>
 		<PhList :size="28" />
 	</a>
 	<a v-else-if="id === 'github'" href="https://github.com/CamelliaCommunity" target="_blank" class="navBarButton" :id>
@@ -18,6 +18,13 @@ const props = defineProps({
 	</a>
 	<a v-else-if="id === 'discord'" href="https://discord.gg/camellia" target="_blank" class="navBarButton" :id>
 		<PhDiscordLogo :size="28" />
+	</a>
+	<a v-else-if="id === 'accountData'" class="navBarButton" :id>
+		<img class="navBarButton" id="accountDataPfp" src="@/assets/images/avatar.png" />
+		<div id="accountDetails">
+			<p id="accountDetails-username">Not logged in!</p>
+			<span id="accountDetails-stats">Log in to access more features...</span>
+		</div>
 	</a>
 </template>
 
@@ -43,6 +50,32 @@ const props = defineProps({
 
 	&:hover {
 		background-color: var(--background-2);
+	}
+
+	&#accountData {
+		width: 244px;
+		background-color: var(--background-3);
+		justify-content: left;
+		text-align: left;
+		line-height: 1.1;
+
+		#accountDetails {
+			flex-direction: column;
+			padding-left: 8px;
+			display: flex;
+			#accountDetails-username {
+				font-weight: 600;
+				font-size: 16px;
+			}
+			#accountDetails-stats {
+				font-size: 12px;
+				font-style: italic;
+			}
+		}
+	}
+	&#accountDataPfp {
+		border-radius: 8px;
+		background-color: transparent;
 	}
 }
 </style>
