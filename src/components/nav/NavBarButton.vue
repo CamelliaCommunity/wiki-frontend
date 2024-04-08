44830870d2b2ba3aab5b995aca7922f5b229a734
Aisuruneko <officialtcgmatt@gmail.com>
2024-04-08 03:01:52 -0400

    add links to & fix navbar buttons

--- src/components/nav/NavBarButton.vue
+++ src/components/nav/NavBarButton.vue
@@ -10,17 +10,15 @@ const props = defineProps({
 </script>
 
 <template>
-	<div class="navBarButton" :id>
-		<a v-if="id === 'menu'">
-			<PhList :size="28" />
-		</a>
-		<a v-if="id === 'github'">
-			<PhGithubLogo :size="28" />
-		</a>
-		<a v-else-if="id === 'discord'">
-			<PhDiscordLogo :size="28" />
-		</a>
-	</div>
+	<a v-if="id === 'menu'" href="#" class="navBarButton" :id>
+		<PhList :size="28" />
+	</a>
+	<a v-else-if="id === 'github'" href="https://github.com/CamelliaCommunity" target="_blank" class="navBarButton" :id>
+		<PhGithubLogo :size="28" />
+	</a>
+	<a v-else-if="id === 'discord'" href="https://discord.gg/camellia" target="_blank" class="navBarButton" :id>
+		<PhDiscordLogo :size="28" />
+	</a>
 </template>
 
 <style lang="scss">
@@ -28,18 +26,23 @@ const props = defineProps({
 	width: 50px;
 	height: 50px;
 	border-radius: 8px;
-	background: var(--background-header-buttons);
+	background-color: var(--background-header-buttons);
 	cursor: pointer;
 	display: flex;
 	justify-content: center;
 	text-align: center;
 	align-items: center;
+	transition: background-color 0.2s ease;
 
 	&#discord {
 		border-radius: 0px 8px 8px 0px;
 	}
 	&#github {
 		border-radius: 8px 0px 0px 8px;
+	}
+
+	&:hover {
+		background-color: var(--background-2);
 	}
 }
 </style>
