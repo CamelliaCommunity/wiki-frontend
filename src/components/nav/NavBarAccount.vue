fd2289ab0e98299737da745d57e60ab879cd1a2e
flustix <me@flux.moe>
2024-04-09 15:55:27 +0200

    Fix avatar not displaying in prod

--- src/components/nav/NavBarAccount.vue
+++ src/components/nav/NavBarAccount.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { reactive } from 'vue';
 
+import DefaultAvatar from '@/assets/images/avatar.png';
+
 // note for whoever is implementing account stuff in the future:
 // you can change the stuff here and it automatically updates the component
 // removes the need to use id's and stuff
 const react = reactive({
-    avatar: "/src/assets/images/avatar.png",
+    avatar: "",
     username: "Not logged in!",
     stats: "Log in to access more features..."
 });
@@ -17,7 +19,7 @@ function onClick() {
 
 <template>
     <div class="w-64 h-12 rounded-lg flex bg-background-3" @click="onClick">
-        <img class="rounded-lg" :src="react.avatar" alt="avatar">
+        <img class="rounded-lg" :src="react.avatar || DefaultAvatar" alt="avatar" />
         <div class="flex flex-col pl-2 text-left justify-center">
             <p class="text-lg leading-none font-medium">{{ react.username }}</p>
             <span class="text-sm leading-none italic opacity-75">{{ react.stats }}</span>
