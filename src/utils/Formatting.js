b994e1b253a063c8d967b1bbd1dea5284588c049
Aisuruneko <officialtcgmatt@gmail.com>
2024-04-13 02:57:48 -0400

    add support for dates and not just unix timestamp

--- src/utils/Formatting.js
+++ src/utils/Formatting.js
@@ -1,5 +1,9 @@
 export default class Formatting {
     static formatDate(secs) {
+		if (isNaN(secs)) {
+			try { secs = (new Date(secs).getTime() / 1000); } catch { return "Invalid Date"; }
+		};
+
         return new Date(secs * 1000).toLocaleDateString('en-GB', {
             day: '2-digit',
             month: 'short',
