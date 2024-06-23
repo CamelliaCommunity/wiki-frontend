b49ea03abcfc6f4c1ad5fb71fe14e716806fcbb8
Aisuruneko <git@nekos.sh>
2024-06-22 20:31:06 -0400

    feat: add permalink icon thingys

--- src/views/article/ArticleView.vue
+++ src/views/article/ArticleView.vue
@@ -2,7 +2,7 @@
 import { useRoute } from 'vue-router';
 import { reactive } from 'vue';
 
-import { PhCaretRight, PhPaperPlaneRight } from '@phosphor-icons/vue';
+import { PhCaretRight, PhLink, PhPaperPlaneRight } from '@phosphor-icons/vue';
 
 import MarkdownView from '@/components/md/MarkdownView.vue';
 import GradientLine from '@/components/GradientLine.vue';
@@ -60,6 +60,50 @@ API.get(articleUrl).then((res) => {
 			const hashToHeader = document.getElementById(route.hash.split("#")[1]);
 			if (hashToHeader) hashToHeader.scrollIntoView();
 		};
+
+		// This will add permalinks in the wackest way.
+		const linkIcon = document.getElementById("template-link-icon");
+		if (linkIcon) {
+			const cLH = (hID) => {
+				const permaLinkHeader = linkIcon.cloneNode(true);
+				permaLinkHeader.style = "";
+				permaLinkHeader.id = "";
+				permaLinkHeader.onclick = (e) => {
+					e.preventDefault();
+					if (navigator.clipboard) {
+						navigator.clipboard.writeText(`${window.location.protocol}//${window.location.host}${window.location.pathname}#${hID}`)
+						.then(() => {
+							// TODO: Show success notification
+						})
+						.catch((err) => {
+							// TODO: Show fail notification
+						})
+					} else {
+						// TODO: Show fail notification
+					};
+				};
+				return permaLinkHeader;
+			};
+
+			const markdownContainer = document.getElementsByClassName("md-content")[0];
+
+			const theH2s = markdownContainer.querySelectorAll("h2");
+			for (const h2 of theH2s) {
+				if (!h2.id) continue;
+				h2.appendChild(cLH(h2.id));
+				h2.classList.add("headerLink");
+			};
+
+			const theH3s = markdownContainer.querySelectorAll("h3");
+			for (const h3 of theH3s) {
+				if (!h3.id) continue;
+				h3.appendChild(cLH(h3.id));
+				h3.classList.add("headerLink");
+			};
+
+			// we are done with the template icon, so remove it.
+			linkIcon.remove();
+		};
 	}, 500);
 });
 // }, 4000); 
@@ -111,6 +155,7 @@ function edit() { // paper smells so we wont use editor (maybe one day?)
 				<!-- comment data would go here -->
 			</div>
 		</ArticleSkeleton>
+		<PhLink id="template-link-icon" style="display:none;" />
     </div>
 </template>
 
@@ -134,6 +179,24 @@ function edit() { // paper smells so we wont use editor (maybe one day?)
 		display: flex;
 		flex-direction: column;
 		gap: 16px;
+	}
+}
+
+h2.headerLink {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	svg {
+		width: 26px;
+		position: relative;
+		top: 3px;
+		cursor: pointer;
+		color: #bbb;
+		transition: color 0.15s ease-in-out;
+		&:hover {
+			color: #fff;
+		}
 	}
 }
 </style>
