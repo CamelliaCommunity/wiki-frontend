6184825e313120dfc46a09ec2ea2e46bce9e1b28
papertek <littlerock894@gmail.com>
2024-08-07 00:48:52 -0500

    comment

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -43,9 +43,8 @@ const react = reactive({
 const latestSongRelease = {
 	meta: {
 		title: "Latest Song Release",
-		// i dunno rewrite this or something
-		// but dont make it a stupid tutorial on how to use a fucking navbar
-		// im going to leave it for now as a placeholder, fuck u - jogn
+		// for now we are lorem
+		// in the figma, theres a video attached to the side. maybe we could add a read more that leads to discography? - john
 		description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 	}
 };
