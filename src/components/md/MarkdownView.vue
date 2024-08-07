d427b23f977158eaa080bd35179622d00139fb03
papertek <littlerock894@gmail.com>
2024-08-07 17:00:48 -0500

    comment

--- src/components/md/MarkdownView.vue
+++ src/components/md/MarkdownView.vue
@@ -47,6 +47,7 @@ onMounted(() => renderMd(props.article));
 
 <style lang="scss">
 .md-content {
+    // TODO: Make things easier to read based on the figma design or wikipedia (padding and margins)
     display: flex;
     flex-direction: column;
     gap: 10px;
