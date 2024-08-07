4ff3c6a78a987b80e94f6e5e24ed0795c7cae12b
papertek <littlerock894@gmail.com>
2024-08-06 20:19:13 -0500

    add default image + remove old tests

--- src/views/home/components/FeaturedPost.vue
+++ src/views/home/components/FeaturedPost.vue
@@ -4,9 +4,8 @@ import { RouterLink } from 'vue-router';
 import OverlapGrid from '@/components/OverlapGrid.vue';
 import LoadingImage from '@/components/LoadingImage.vue';
 
-import TestImage from '@/assets/images/test-image-sherii.png';
-import TestImage2 from '@/assets/images/test-image-banginburst.png';
 import EmptyImage from '@/assets/images/empty.png';
+import DefaultImage from '@/assets/images/test-image-default.png';
 
 import Formatting from '@/utils/Formatting';
 
@@ -21,17 +20,13 @@ const props = defineProps({
     post: {
         type: Object,
         required: true
-    },
-    otherImage: {
-        type: Boolean,
-        default: false
     }
 });
 </script>
 
 <template>
     <OverlapGrid class="featured-post w-full h-52 rounded-lg" v-if="post.meta">
-        <LoadingImage :src="post.meta.image || (otherImage ? TestImage2 : TestImage) || EmptyImage" class="object-cover" />
+        <LoadingImage :src="post.meta.image || DefaultImage || EmptyImage" class="object-cover" />
         <div class="dim" v-if="linearBackground"></div>
         <div class="radial-dim" v-else></div>
         <div class="card-content">
