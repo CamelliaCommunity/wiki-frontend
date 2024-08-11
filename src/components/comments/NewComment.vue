a41370d4559f546a97d4da26a30996459e7b5841
papertek <littlerock894@gmail.com>
2024-08-10 20:36:53 -0500

    adjust comments more

--- src/components/comments/NewComment.vue
+++ src/components/comments/NewComment.vue
@@ -6,7 +6,7 @@ import API from '@/utils/API';
 <template>
 	<div class="new-comment">
 		<div class="w-full h-16 rounded flex gap-3">
-			<img class="rounded-xl size-16 object-fill" :src="API.user.avatar" alt="avatar" />
+			<img class="rounded-xl size-19 object-fill" :src="API.user.avatar" alt="avatar" />
 			<div class="h-fit w-full flex flex-col rounded-xl bg-background-3 p-1 gap-1">
 				<div class="w-full flex gap-1">
 					<textarea class="h-10 w-full resize-none overflow-hidden rounded-lg bg-background-2 px-3 py-1.5 text-base outline-none" placeholder="Soon(TM)  Press enter to post. Use shift+enter to make a new line." disabled />
