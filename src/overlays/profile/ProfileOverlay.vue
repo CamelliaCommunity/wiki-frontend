1c0c2c03c9d0e8332e526535642a4f1c61b442a1
papertek <littlerock894@gmail.com>
2024-08-16 02:09:59 -0500

    good color

--- src/overlays/profile/ProfileOverlay.vue
+++ src/overlays/profile/ProfileOverlay.vue
@@ -37,7 +37,7 @@ function Close(e) {
     <Transition name="overlay">
         <div class="z-50 flex fixed justify-center items-center w-screen h-screen top-0 py-24 bg-black bg-opacity-25 backdrop-blur-sm overflow-y-scroll"
             @click="Close" v-if="react.open">
-            <div class="z-0 w-content-width min-h-full bg-background-2 bg-opacity-80 shadow-xl rounded-xl flex flex-col items-center justify-center" ref="content">
+            <div class="z-0 w-content-width min-h-full bg-background-1 bg-opacity-80 shadow-xl rounded-xl flex flex-col items-center justify-center" ref="content">
                 <p class="text-3xl mb-2">welcome to the profile overlay!</p>
                 <p class="text-lg leading-none opacity-80">unfortunately nothing is here yet...</p>
                 <p class="text-lg leading-none opacity-80">maybe come back another time?</p>
