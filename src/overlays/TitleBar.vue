fd09fbf6a0edb55be1684afeba2e8a4b5e50e5b7
papertek <littlerock894@gmail.com>
2024-09-09 00:07:27 -0500

    edit profile modal

--- src/overlays/TitleBar.vue
+++ src/overlays/TitleBar.vue
@@ -10,8 +10,10 @@ defineProps({
 
 <template>
 	<div class="flex w-full mb-2">
-		<p class="text-4xl font-bold w-full">Profile</p>
-		<div class="bg-background-3 p-3 flex justify-center items-center rounded-lg hover:bg-background-4 theShadow cursor-pointer" @click="close(null)">
+		<!-- really want to make this only display when you click on profiles and not the profile of the user themself - john -->
+		<p class="text-4xl font-bold w-full">Profile View</p>
+		<div class="bg-background-3 p-3 flex justify-center items-center rounded-lg hover:bg-background-4 theShadow cursor-pointer"
+			@click="close(null)">
 			<PhX :size="16" @click="close(null)" />
 		</div>
 	</div>
