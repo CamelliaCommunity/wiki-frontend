ebb3141b7f8576c29e8ac126f7713158d5bd7d22
t404owo <tags4owo@gmail.com>
2024-09-11 22:31:03 +0200

    added some routes and some files/folders (yesterday); added some stuffs into SettingsView

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/home/HomeView.vue'
-import ArticleView from '../views/article/ArticleView.vue'
-import oauthComplete from '../views/oauthComplete.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/home/HomeView.vue';
+import SettingsView from '../views/settings/SettingsView.vue';
+import PollsView from '../views/polls/PollsView.vue';
+import ArticleView from '../views/article/ArticleView.vue';
+import oauthComplete from '../views/oauthComplete.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,19 +13,29 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-	{
+    {
+      path: '/settings',
+      name: 'settings',
+      component: SettingsView
+    },
+    {
+      path: '/polls',
+      name: 'polls',
+      component: PollsView
+    }, //add /{id} soon
+	  {
       path: '/oauthComplete',
       name: 'oauthComplete',
       component: oauthComplete
-	},
-	{ // redirect everything else to article
+	  },
+	  { // redirect everything else to article
       path: '/:pathMatch(.*)*',
       component: ArticleView
-    }
+    },
   ],
   scrollBehavior: (to, from, savedPosition) => {
     return { top: 0 }
   }
-})
+});
 
-export default router
+export default router;
---
+++ src/views/polls/PollsView.vue
@@ -0,0 +1,6 @@
+<script setup>
+// pretty much wips here xD
+</script>
+<template>
+    idk test test?
+</template>
