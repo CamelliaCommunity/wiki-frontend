cf33cb859c0da4a64b8e977d71d3e6861ad22c1a
t404owo <tags4owo@gmail.com>
2024-09-11 23:18:48 +0200

    Fixed some alignments and font sizes

--- src/views/settings/SettingsView.vue
+++ src/views/settings/SettingsView.vue
@@ -21,43 +21,43 @@ gotta base on working this thing bcz it might require 3 sites due to options...
 
 		<div class="flex flex-row w-full xl:w-content-width max-h-full mx-auto py-8">
 			<div class="flex-column">
-				<div class="flex flex-row cursor-pointer py-2 pl-4 pr-20 border-l-2
-				 border-l-background-2 hover:border-l-accent">
+				<div class="flex flex-row items-center cursor-pointer py-2 pl-4 pr-20 
+         border-l-2 border-l-background-2 hover:border-l-accent">
 					<PhUserGear class="size-8"></PhUserGear>
 					<div class="px-2">
 						<p class="text-xl font-medium">General</p>
-						<p>Edit General Settings</p>
+						<p class="test-lg">Edit General Settings</p>
 					</div>
 				</div>
-				<div class="flex flex-row cursor-pointer py-2 pl-4 pr-20 border-l-2
-				 border-l-background-2 hover:border-l-accent">
+				<div class="flex flex-row items-center cursor-pointer py-2 pl-4 pr-20 
+         border-l-2 border-l-background-2 hover:border-l-accent">
 					<PhGithubLogo class="size-8"></PhGithubLogo>
 					<div class="px-2">
 						<p class="text-xl font-medium">Github</p>
-						<p>Link you GitHub Account</p>
+						<p class"test-lg">Link you GitHub Account</p>
 					</div>
 				</div>
-				<div class="flex flex-row cursor-pointer py-2 pl-4 pr-20 border-l-2
-				 border-l-background-2 hover:border-l-accent">
+				<div class="flex flex-row items-center cursor-pointer py-2 pl-4 pr-20 
+         border-l-2 border-l-background-2 hover:border-l-accent">
 					<PhLockKey class="size-8"></PhLockKey>
 					<div class="px-2">
 						<p class="text-xl font-medium">Privacy</p>
-						<p>Edit Account Privacy</p>
+						<p class"test-lg">Edit Account Privacy</p>
 					</div>
 				</div>
 		    	<div class="gray-line opacity-40"></div>
-				<div class="flex flex-row cursor-pointer py-2 px-2 pl-4 border-l-2
-				 border-l-background-2 text-red fill-red hover:fill-accent-soft
+				<div class="flex flex-row items-center cursor-pointer py-2 px-2 pl-4
+         border-l-2 border-l-background-2 text-red fill-red hover:fill-accent-soft
 				  hover:border-l-accent-soft hover:text-accent-soft">
 					<PhSignOut class="size-8"></PhSignOut>
 					<div class="px-2">
 						<p class="text-xl font-medium">Logout</p>
-						<p>Log out of your Account</p>
+						<p class"test-lg">Log out of your Account</p>
 					</div>
 				</div>
 			</div>
 			<div class="flex-column  pl-2">
-				<p class="">You may only edit profile pictures and banners in Discord!</p>
+				<p class="text-lg">You may only edit profile pictures and banners in Discord!</p>
 			</div>
 		</div>
 	</div>
