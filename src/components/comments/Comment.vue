4e2795b1da9750f8ad12a6332790a800e1340521
Aisuruneko <git@nekos.sh>
2024-09-14 10:54:39 -0400

    more comment layout stuff (no real data yet)

--- src/components/comments/Comment.vue
+++ src/components/comments/Comment.vue
@@ -1,8 +1,12 @@
 <script setup>
 import { reactive } from 'vue';
+import { useRoute } from 'vue-router';
+
 import API from '@/utils/API';
+import Toast from '@/utils/Toast';
 import { PhArrowFatUp, PhArrowFatDown, PhArrowBendUpLeft, PhDotsThree, PhPencil, PhTrash, PhLink, PhFlag } from '@phosphor-icons/vue';
 
+const route = useRoute();
 const props = defineProps({
 	user: {
 		type: Object,
@@ -19,12 +23,28 @@ const comment = reactive({
 });
 
 const chngMoreActions = () => comment.moreActions = !comment.moreActions;
+const changeHover = (isOpen) => { comment.hovered = isOpen; if (!isOpen) comment.moreActions = false; };
 
+const commentAction = (action) => {
+	if (action == 4) {
+		const url = `${route.fullPath.split("#")[0]}#comment-${comment.commentId}`;
+
+		if (!navigator.clipboard) {
+			Toast.showToast("Your browser does not support copying to the clipboard, sorry.")
+			return;
+		}
+
+		navigator.clipboard.writeText(`${window.location.protocol}//${window.location.host}${url}`)
+			.then(() => Toast.showToast("Copied to clipboard!", { type: "success" }))
+			.catch((err) => Toast.showToast("Error occurred copying to clipboard."));
+	};
+};
 </script>
 
 <template>
-	<div class="bg-background-2 hover:bg-background-3 rounded-xl w-full max-w-screen-lg min-h-24 gap-3 px-2 py-2 flex m-auto"
-		@mouseover="comment.hovered = true" @mouseleave="comment.hovered = false">
+	<div :id="`comment-${comment.commentId}`"
+		class="bg-background-2 hover:bg-background-3 rounded-xl w-full max-w-screen-lg min-h-24 gap-3 px-2 py-2 flex m-auto"
+		@mouseover="changeHover(true)" @mouseleave="changeHover(false)">
 		<div class="flex flex-col gap-2">
 			<img :class="`rounded-2xl border-2 h-14 min-w-fit`" :src="user.avatar"
 				:style="`border-color: ${user.color}`" alt="avatar" />
@@ -41,14 +61,14 @@ const chngMoreActions = () => comment.moreActions = !comment.moreActions;
 				<div class="bg-background-1 rounded-md hover:bg-background-2 cursor-pointer w-fit"
 					v-if="comment.hovered">
 					<PhDotsThree :size="24" @click="chngMoreActions" />
-					<div class="absolute mt-1 h-fit flex flex-col bg-background-1 w-fit py-2 rounded-md"
+					<div class="absolute mt-1 h-fit flex flex-col bg-background-1 w-fit rounded-md"
 						v-if="comment.moreActions">
-						<span
-							class="flex text-light-gray cursor-pointer align-middle items-center gap-1 text-base rounded-sm px-2 hover:text-white">
+						<span @click="commentAction(4)"
+							class="flex text-light-gray cursor-pointer align-middle items-center gap-1 text-base rounded-sm px-2 hover:text-white hover:bg-gray py-1">
 							<PhLink /> Link
 						</span>
-						<span
-							class="flex text-red cursor-pointer align-middle items-center gap-1 text-base rounded-sm px-2 hover:text-white">
+						<span @click="commentAction(5)"
+							class="flex text-red cursor-pointer align-middle items-center gap-1 text-base rounded-sm px-2 hover:text-white hover:bg-gray py-1">
 							<PhFlag /> Report
 						</span>
 					</div>
@@ -62,15 +82,15 @@ const chngMoreActions = () => comment.moreActions = !comment.moreActions;
 					<PhArrowFatDown class="cursor-pointer hover:text-white" :size="24" @click="comment.votes--" />
 				</div>
 				<div class="flex" v-if="comment.hovered">
-					<span
+					<span @click="commentAction(1)"
 						class="flex text-light-gray cursor-pointer align-middle items-center gap-1 text-lg rounded-sm px-2 hover:text-white">
 						<PhArrowBendUpLeft /> Reply
 					</span>
-					<span
+					<span @click="commentAction(2)"
 						class="flex text-light-gray cursor-pointer align-middle items-center gap-1 text-lg rounded-sm px-2 hover:text-white">
 						<PhPencil /> Edit
 					</span>
-					<span
+					<span @click="commentAction(3)"
 						class="flex text-red cursor-pointer align-middle items-center gap-1 text-lg rounded-sm px-2 hover:text-white">
 						<PhTrash /> Delete
 					</span>
