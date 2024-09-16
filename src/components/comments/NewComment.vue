02401e2146e82eeffa8e489b74633c502b2cee8f
Aisuruneko <git@nekos.sh>
2024-09-16 19:47:56 -0400

    refactor: a little bit of new comment & the textbox (separation)

--- src/components/comments/NewComment.vue
+++ src/components/comments/NewComment.vue
@@ -1,5 +1,9 @@
 <script setup>
+import { nextTick, ref, shallowRef } from 'vue';
+import { PhPaperPlaneTilt, PhXCircle, PhCheckCircle, PhArrowClockwise } from '@phosphor-icons/vue';
+
 import API from '@/utils/API';
+import Toast from '@/utils/Toast';
 
 import Textbox from '../textbox/Textbox.vue';
 
@@ -13,13 +17,94 @@ const props = defineProps({
 		default: false
 	}
 });
+
+const knownStates = {
+	NORMAL: { icon: PhPaperPlaneTilt, spin: false },
+	SUBMITTING: { icon: PhArrowClockwise, spin: true },
+	OK: { icon: PhCheckCircle, spin: false },
+	ERROR: { icon: PhXCircle, spin: false },
+};
+
+// let beDisabled = ref(API.user.loggedIn ? props.beDisabled : true);
+const beDisabled = ref(false);
+const submitIconClasses = ref("");
+const currentState = shallowRef(knownStates.NORMAL);
+const previousState = shallowRef(knownStates.NORMAL);
+
+// shift+tab handling
+const handleKeydown = (e) => {
+	if (e.keyCode === 13 && e.shiftKey) {
+		textareaHeight(e);
+	}
+	else if (e.keyCode === 13) { // if da user press enter it should SUBMIT
+		submitComment();
+		e.preventDefault();
+	}
+};
+
+const handleInput = (e) => {
+	textareaHeight(e);
+};
+
+const textareaHeight = (e) => {
+	e.target.style.height = "";
+	e.target.style.height = e.target.scrollHeight + "px";
+};
+
+const updateTextbox = () => {
+	nextTick(() => {
+		submitIconClasses.value = submitIconClasses.value.replace("spin-spin-spin", "");
+		if (currentState.value == knownStates.SUBMITTING) {
+			submitIconClasses.value = "spin-spin-spin";
+		};
+	});
+};
+const updateState = (newState) => {
+	previousState.value = currentState.value;
+	currentState.value = newState;
+};
+
+const submitComment = () => {
+	if (beDisabled.value) return;
+	if (currentState.value != knownStates.NORMAL) return;
+
+	updateState(knownStates.SUBMITTING);
+	beDisabled.value = true;
+	updateTextbox();
+
+	setTimeout(() => {
+		updateState(knownStates.ERROR);
+		beDisabled.value = false;
+		updateTextbox();
+		Toast.showToast("An internal error occurred while processing your request.", { type: "error" });
+
+		setTimeout(() => {
+			updateState(knownStates.SUBMITTING);
+			beDisabled.value = true;
+			updateTextbox();
+			setTimeout(() => {
+				updateState(knownStates.OK);
+				updateTextbox();
+
+				setTimeout(() => {
+					updateState(knownStates.NORMAL);
+					beDisabled.value = false;
+					updateTextbox();
+				}, 1000);
+			}, 2000);
+		}, 2000);
+	}, 2000);
+};
+
 </script>
 
 <template>
 	<div class="new-comment">
 		<div class="w-full rounded flex gap-3">
 			<img class="rounded-xl object-fill max-h-24 my-auto" :src="API.user.avatar" alt="avatar" />
-			<Textbox :be-disabled="loaded ? error : true"></Textbox>
+			<Textbox :be-disabled="beDisabled" :handleInput="handleInput" :handleKeydown="handleKeydown"
+				:handleSubmit="submitComment" :submitIcon="currentState.icon" :submitIconClasses="submitIconClasses">
+			</Textbox>
 		</div>
 	</div>
 </template>
--- src/components/textbox/Textbox.vue
+++ src/components/textbox/Textbox.vue
@@ -1,9 +1,8 @@
 <script setup>
-import { nextTick, ref, shallowRef } from 'vue';
-import { PhPaperPlaneTilt, PhXCircle, PhCheckCircle, PhArrowClockwise, PhTextBolder, PhTextItalic, PhTextH, PhTextUnderline, PhTextStrikethrough, PhListBullets, PhListNumbers, PhListChecks, PhQuotes, PhCodeSimple, PhHighlighter, PhSelection, PhGridFour, PhLinkSimple, PhImage } from '@phosphor-icons/vue';
+import { toRefs } from 'vue';
 
-import API from '@/utils/API';
-import Toast from '@/utils/Toast';
+import { PhTextBolder, PhTextItalic, PhTextH, PhTextUnderline, PhTextStrikethrough, PhListBullets, PhListNumbers, PhListChecks, PhQuotes, PhCodeSimple, PhHighlighter, PhSelection, PhGridFour, PhLinkSimple, PhImage, PhPaperPlaneRight } from '@phosphor-icons/vue';
+
 import TextboxIcon from './TextboxIcon.vue';
 
 const props = defineProps({
@@ -14,87 +13,31 @@ const props = defineProps({
 	beDisabled: {
 		type: Boolean,
 		default: false
+	},
+	submitIcon: {
+		type: Object,
+		default: PhPaperPlaneRight
+	},
+	submitIconClasses: {
+		type: String,
+		default: "",
+		required: false
+	},
+	handleInput: {
+		type: Function,
+		default: () => { }
+	},
+	handleKeydown: {
+		type: Function,
+		default: () => { }
+	},
+	handleSubmit: {
+		type: Function,
+		default: () => { }
 	}
 });
 
-const knownStates = {
-	NORMAL: { icon: PhPaperPlaneTilt },
-	SUBMITTING: { icon: PhArrowClockwise },
-	OK: { icon: PhCheckCircle },
-	ERROR: { icon: PhXCircle },
-};
-
-let beDisabled = ref(API.user.loggedIn ? props.beDisabled : true);
-let currentState = shallowRef(knownStates.NORMAL);
-let previousState = shallowRef(knownStates.NORMAL);
-
-// shift+tab handling
-const handleKeydown = (e) => {
-	if (e.keyCode === 13 && e.shiftKey) {
-		textareaHeight(e);
-	}
-	else if (e.keyCode === 13) { // if da user press enter it should SUBMIT
-		submitComment();
-		e.preventDefault();
-	}
-};
-
-const handleInput = (e) => {
-	textareaHeight(e);
-};
-
-const textareaHeight = (e) => {
-	e.target.style.height = "";
-	e.target.style.height = e.target.scrollHeight + "px";
-};
-
-const updateTextbox = () => {
-	nextTick(() => {
-		const submitBtn = document.getElementById("submit");
-		submitBtn.children[0].classList[currentState.value == knownStates.SUBMITTING ? "add" : "remove"]("spin-spin-spin");
-	});
-};
-const updateState = (newState) => {
-	previousState.value = currentState.value;
-	currentState.value = newState;
-};
-
-const submitComment = () => {
-	if (beDisabled) return;
-	if (currentState.value != knownStates.NORMAL) return;
-
-	updateState(knownStates.SUBMITTING);
-	beDisabled = true;
-	updateTextbox();
-
-	setTimeout(() => {
-		updateState(knownStates.ERROR);
-		beDisabled = false;
-		updateTextbox();
-		Toast.showToast("An internal error occurred while processing your request.", { type: "error" });
-
-		setTimeout(() => {
-			updateState(knownStates.SUBMITTING);
-			beDisabled = true;
-			updateTextbox();
-			setTimeout(() => {
-				updateState(knownStates.OK);
-				updateTextbox();
-
-				setTimeout(() => {
-					updateState(knownStates.NORMAL);
-					beDisabled = false;
-					updateTextbox();
-				}, 1000);
-			}, 2000);
-		}, 2000);
-	}, 2000);
-};
-
-nextTick(() => {
-	beDisabled = !API.user.loggedIn;
-});
-
+const { handleInput, handleKeydown, handleSubmit } = toRefs(props);
 </script>
 
 <template>
@@ -106,15 +49,15 @@ nextTick(() => {
 				@keydown="handleKeydown" :disabled="beDisabled" />
 
 			<div :class='"m-auto flex size-10 items-center justify-center rounded-lg bg-background-4 p-1 cursor-" + `${beDisabled ? "deny" : "pointer"}`'
-				id="submit" @click="submitComment" :disabled="beDisabled">
-				<Component :is="currentState.icon" :size="20" :disabled="beDisabled"></Component>
+				id="submit" @click="handleSubmit" :disabled="beDisabled">
+				<Component :is="submitIcon" :size="20" :disabled="beDisabled" :class="submitIconClasses"></Component>
 			</div>
 		</div>
 		<div class="max-w-fit h-10 flex px-2 gap-2 bg-background-2 rounded-lg py-2">
 			<div id="formatting" class="w-auto flex gap-3">
 				<TextboxIcon :icon="PhTextBolder" :disabled="beDisabled" />
 				<TextboxIcon :icon="PhTextItalic" :disabled="beDisabled" />
-				<TextboxIcon :icon="PhTextH" :disabled="beDisabled" />
+				<TextboxIcon v-if="isEditor" :icon="PhTextH" :disabled="beDisabled" />
 				<TextboxIcon :icon="PhTextUnderline" :disabled="beDisabled" />
 				<TextboxIcon :icon="PhTextStrikethrough" :disabled="beDisabled" />
 			</div>
@@ -122,15 +65,15 @@ nextTick(() => {
 			<div id="list" class="w-auto flex gap-3">
 				<TextboxIcon :icon="PhListBullets" :disabled="beDisabled" />
 				<TextboxIcon :icon="PhListNumbers" :disabled="beDisabled" />
-				<TextboxIcon :icon="PhListChecks" :disabled="beDisabled" />
+				<TextboxIcon v-if="isEditor" :icon="PhListChecks" :disabled="beDisabled" />
 			</div>
 			<div class="h-auto w-0.5" style="background: rgba(255, 255, 255, 15%);"></div>
 			<div id="misc" class="w-auto flex gap-3">
 				<TextboxIcon :icon="PhQuotes" :disabled="beDisabled" />
-				<TextboxIcon :icon="PhCodeSimple" :disabled="beDisabled" />
-				<TextboxIcon :icon="PhHighlighter" :disabled="beDisabled" />
+				<TextboxIcon v-if="isEditor" :icon="PhCodeSimple" :disabled="beDisabled" />
+				<TextboxIcon v-if="isEditor" :icon="PhHighlighter" :disabled="beDisabled" />
 				<TextboxIcon :icon="PhSelection" :disabled="beDisabled" />
-				<TextboxIcon :icon="PhGridFour" :disabled="beDisabled" />
+				<TextboxIcon v-if="isEditor" :icon="PhGridFour" :disabled="beDisabled" />
 				<TextboxIcon :icon="PhLinkSimple" :disabled="beDisabled" />
 				<TextboxIcon :icon="PhImage" :disabled="beDisabled" />
 			</div>
