280302760ec3fbc0dbce5a1ce4f7374b0ec7f2ea
papertek <littlerock894@gmail.com>
2024-09-28 05:39:13 -0500

    improve layout some more (wip)

--- src/views/news/NewsView.vue
+++ src/views/news/NewsView.vue
@@ -14,6 +14,7 @@ import GradientLine from '@/components/GradientLine.vue';
 import { PhCaretRight } from '@phosphor-icons/vue';
 import WikiLogo from "@/assets/images/icon.png";
 import Kofi from "@/assets/images/kofi.png";
+import GrayLine from '@/components/GrayLine.vue';
 
 Utils.setTitle('News');
 
@@ -85,26 +86,26 @@ API.get("/articles/recent?type=community&count=5").then((res) => {
 </script>
 
 <template>
-	<div class="w-full flex flex-col items-center justify-center gap-5">
+	<div class="w-full flex flex-col items-center justify-center">
+		<div class="xl:w-content-width flex flex-col items-center justify-center">
+			<div class="flex justify-between w-full mb-2 px-5">
+				<p class="flex gap-0.5 items-center">
+					<RouterLink to="/" class="text-light-gray readMoreHover">Home</RouterLink>
+					<span class="flex items-center gap-1">
+						<PhCaretRight :size="16" class="text-light-gray" />
+						<RouterLink to="/news" class="text-light-gray readMoreHover">News</RouterLink>
+					</span>
+				</p>
+			</div>
+			<div
+				class="w-full md:h-16 bg-background-1 rounded-lg p-5 flex flex-col md:flex-row justify-between items-center mb-4">
+				<h3 class="text-2xl font-semibold">The Wiki Times</h3>
+				<p class="font-extralight">The only Real News Source</p>
+				<img :src="WikiLogo" alt="Camellia Wiki Icon" class="w-8" />
+			</div>
+		</div>
 		<div class="flex flex-col md:flex-row w-full xl:w-content-width xl:mx-auto gap-5 md">
 			<div class="flex w-full flex-col gap-4">
-				<div class="flex justify-between w-full px-5 mb-0">
-					<p class="flex gap-0.5 items-center">
-						<RouterLink to="/" class="text-light-gray readMoreHover">Homepage</RouterLink>
-						<span class="flex items-center gap-1">
-							<PhCaretRight :size="16" class="text-light-gray" />
-							<RouterLink to="/news" class="text-light-gray readMoreHover">News</RouterLink>
-						</span>
-					</p>
-				</div>
-				<div
-					class="w-full md:h-16 bg-background-1 rounded-lg p-5 flex flex-col md:flex-row justify-between items-center mb-4">
-					<h3 class="text-2xl font-semibold">The Wiki Times</h3>
-					<p class="font-extralight">The only Real News Source</p>
-					<img :src="WikiLogo" alt="Camellia Wiki Icon" class="w-8" />
-				</div>
-
-
 				<div class="w-full flex flex-col md:flex-row gap-4">
 					<BigPost post-type="Popular Today" :post="react.popular" linearBackground other-image />
 					<div class="flex w-full flex-col gap-4">
@@ -118,22 +119,24 @@ API.get("/articles/recent?type=community&count=5").then((res) => {
 					<p>On a serious note, we need money.</p>
 					<img :src="Kofi" class="w-80 m-auto">
 				</BlockquoteNote>
-				<BlockquoteNote class="border-x-0 rounded-lg" title="Community Advertisement">
-					<p class='text-2xl font-semibold'>Follow Spinny's Twitter. It's our therapy.
-						<br>
-						<a class="text-lg text-accent" target="_blank" href="https://x.com/spinnyternally">
-							Visit Spinny's Twitter
-						</a>
-					</p>
-				</BlockquoteNote>
-				<div>
-					<h2 class="text-4xl font-semibold">Read More</h2>
-					<GradientLine :overshoot="false" />
-				</div>
-				<div class="w-full md:grid-cols-2 gap-4 px-4">
-					<div v-for="(post) in react.news" class="w-full flex flex-col gap-4">
-						<SimplePost :post="post" />
-
+				<div class="flex w-full flex-col gap-4 px-8">
+					<BlockquoteNote class="border-x-0 rounded-lg" title="Community Advertisement">
+						<p class='text-2xl font-semibold'>Follow Spinny's Twitter. It's our therapy.
+							<br>
+							<a class="text-lg text-accent" target="_blank" href="https://x.com/spinnyternally">
+								Visit Spinny's Twitter
+							</a>
+						</p>
+					</BlockquoteNote>
+					<div>
+						<h2 class="text-4xl font-semibold">Read More</h2>
+						<GradientLine :overshoot="false" />
+					</div>
+					<div class="w-full flex flex-col gap-2 lg:max-h-full xl:max-h-96 overflow-y-auto max-h-full">
+						<div v-for="(post, index) in react.news" class="flex flex-col w-full gap-2">
+							<SimplePost :post="post" />
+							<GrayLine v-if="index != (react.news.length - 1)" />
+						</div>
 					</div>
 				</div>
 			</div>
