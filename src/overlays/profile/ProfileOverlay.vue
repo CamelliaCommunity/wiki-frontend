a44499f9d16d6251bfe052b451c1f06262a2f738
Ayano <s.correa.wilches@gmail.com>
2024-10-09 21:38:08 -0500

    create base profile overlay

--- src/overlays/profile/ProfileOverlay.vue
+++ src/overlays/profile/ProfileOverlay.vue
@@ -1,125 +1,202 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import { reactive, ref } from "vue";
 
-import Events from '@/utils/Events';
-import ActiveComponents from '@/utils/ActiveComponents';
-import API from '@/utils/API';
-import Toast from '@/utils/Toast';
+import Events from "@/utils/Events";
+import ActiveComponents from "@/utils/ActiveComponents";
+import API from "@/utils/API";
+import Toast from "@/utils/Toast";
+import GradientLine from "@/components/GradientLine.vue";
+import GrayLine from "@/components/GrayLine.vue";
 
-import TitleBar from '../TitleBar.vue';
-import PopupOverlay from '../popup/PopupOverlay.vue';
-import Button from '@/components/Button.vue';
+import TitleBar from "../TitleBar.vue";
+import PopupOverlay from "../popup/PopupOverlay.vue";
+import Button from "@/components/Button.vue";
+import ProfileComment from "./ProfileComment.vue";
 
 const content = ref();
 
 const react = reactive({
-	open: false,
-	user: {}
+  open: false,
+  user: {},
 });
 
 const CacheSystem = {
-	users: {},
-	refresh: 15, // In seconds
+  users: {},
+  refresh: 15, // In seconds
 };
 
-Events.Register('profile-overlay', (userID) => {
-	let toRefresh = true;
-	if (CacheSystem[userID]) {
-		if (Date.now() < CacheSystem[userID].refreshedAt + (CacheSystem.refresh * 1000))
-			toRefresh = false;
-	};
-	console.log("looking up user using cache? " + ((CacheSystem[userID] !== undefined) ? "yes" : "no"));
+const userStatistics = [
+  {
+    label: "Join Date",
+    value: "Jan 28th, 2024",
+  },
+  {
+    label: "Articles Written",
+    value: 0,
+  },
+  {
+    label: "Comments",
+    value: 0,
+  },
+];
 
-	if (toRefresh) {
-		let profileData = API.get(`/profile/${userID}`);
-		CacheSystem[userID] = {
-			data: profileData,
-			refreshedAt: Date.now()
-		};
-		react.user = profileData;
-	};
+Events.Register("profile-overlay", (userID) => {
+  let toRefresh = true;
+  if (CacheSystem[userID]) {
+    if (
+      Date.now() <
+      CacheSystem[userID].refreshedAt + CacheSystem.refresh * 1000
+    )
+      toRefresh = false;
+  }
+  console.log(
+    "looking up user using cache? " +
+      (CacheSystem[userID] !== undefined ? "yes" : "no")
+  );
 
-	react.open = true;
-	ActiveComponents.open("profile-overlay");
+  if (toRefresh) {
+    let profileData = API.get(`/profile/${userID}`);
+    CacheSystem[userID] = {
+      data: profileData,
+      refreshedAt: Date.now(),
+    };
+    react.user = profileData;
+  }
+
+  react.open = true;
+  ActiveComponents.open("profile-overlay");
 });
 
 Events.Register("profile-overlay-close", () => {
-	Close(null);
+  Close(null);
 });
 
 function Close(e) {
-	if (e && content?.value?.contains(e.target))
-		return;
+  if (e && content?.value?.contains(e.target)) return;
 
-	react.open = false;
-	react.user = {};
+  react.open = false;
+  react.user = {};
 }
 
 // temporary function - john
 function wipToast() {
-	Toast.showToast("That feature is not implemented yet but will be soon!", { type: "error" })
+  Toast.showToast("That feature is not implemented yet but will be soon!", {
+    type: "error",
+  });
 }
 
-// logs out then closes the popup - john
-function logOut(ClosePopup) {
-	if (API.user.loggedIn) {
-		API.performLogout();
-		ClosePopup();
-	}
-}
+API.user.avatar = "https://picsum.photos/56/56";
 </script>
 
 <template>
-	<Transition name="overlay">
-		<div class="z-50 flex fixed justify-center items-center w-screen h-screen top-0 py-24 bg-opacity-25 backdrop-blur overflow-y-scroll"
-			@click="Close" v-if="react.open">
-			<div class="z-0 w-content-width min-h-full bg-background-1 bg-opacity-90 backdrop-blur theShadow rounded-xl flex flex-col p-5 gap-1"
-				ref="content">
-				<TitleBar title="Profile View" :close="Close" />
-				<div class="flex grow">
-					<div class="w-full flex mb-2.5"></div>
-				</div>
-				<!-- the log out button is temporary - john -->
-				<p class="text-lg text-red bottom-0 justify-center mx-auto cursor-pointer"
-					@click="Events.Emit('popup-logout')">
-					Log Out</p>
-				<p class="text-lg text-red bottom-0 justify-center mx-auto cursor-pointer" @click="wipToast">Report
-					Profile</p>
-			</div>
-		</div>
-	</Transition>
-
-	<PopupOverlay event="popup-logout">
-		<template #content>Are you very sure you want to logout?</template>
-		<template #footer="{ ClosePopup }">
-			<div class="flex justify-center gap-2">
-				<Button type="success" @click="logOut(ClosePopup)">Yes</Button>
-				<Button type="error" @click="ClosePopup">Cancel</Button>
-			</div>
-		</template>
-	</PopupOverlay>
+  <Transition name="overlay">
+    <div
+      class="z-50 flex fixed justify-center items-center w-screen h-screen top-0 py-24 bg-opacity-25 backdrop-blur overflow-y-scroll"
+      @click="Close"
+      v-if="react.open"
+    >
+      <div
+        class="z-0 w-content-width min-h-full bg-background-1 bg-opacity-90 backdrop-blur theShadow rounded-xl flex flex-col p-5 gap-1"
+        ref="content"
+      >
+        <TitleBar title="Profile View" :close="Close" />
+        <div class="flex flex-row items-center gap-2">
+          <img
+            :class="`rounded-xl border-2`"
+            :src="API.user.avatar"
+            :style="`border-color: ${API.user.color}`"
+            alt="avatar"
+          />
+          <div class="flex flex-col">
+            <span class="text-2xl font-semibold">Name</span>
+            <span class="text-sm font-semibold opacity-80">@ID</span>
+          </div>
+        </div>
+        <div class="grid grid-cols-4 gap-8">
+          <div class="col-span-3 flex flex-col gap-4">
+            <div class="flex flex-row gap-2">
+              <span>Comments</span>
+              <span>Polls</span>
+            </div>
+            <GradientLine :overshoot="false" />
+            <div class="flex flex-row gap-4">
+              <span>Sort by</span>
+              <div class="flex flex-row gap-2">
+                <span>New</span>
+                <span>Old</span>
+              </div>
+            </div>
+            <div class="flex flex-col gap-4 overflow-auto"></div>
+          </div>
+          <div class="col-span-1">
+            <div class="flex flex-col gap-2">
+              <div class="flex flex-row items-center justify-between">
+                <span class="justify text-2xl font-semibold">About Me</span>
+                <span>Edit</span>
+              </div>
+              <GradientLine :overshoot="false" />
+              <span class="text-xl leading-6 font-light"
+                >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit
+                quisquam laudantium architecto dolor ipsam temporibus officia
+                porro reiciendis eaque totam necessitatibus fugiat quae non
+                delectus sapiente omnis enim, nemo officiis!</span
+              >
+              <GrayLine :overshoot="false" />
+              <div
+                class="flex flex-col gap-1"
+                v-for="statistic in userStatistics"
+              >
+                <span class="text-lg font-medium leading-4">{{
+                  statistic.label
+                }}</span>
+                <span class="text-base light leading-4 opacity-70">{{
+                  statistic.value
+                }}</span>
+              </div>
+              <div class="flex flex-col gap-1">
+                <span class="text-lg font-medium leading-4">Connections</span>
+                <div class="flex flex-row items-center gap-2">
+                  <span>D</span>
+                  <span>G</span>
+                  <span>C</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p
+          class="text-lg text-red bottom-0 justify-center mx-auto cursor-pointer"
+          @click="wipToast"
+        >
+          Report Profile
+        </p>
+      </div>
+    </div>
+  </Transition>
 </template>
 
 <style>
 .overlay-enter-active,
 .overlay-leave-active {
-	transition: opacity 150ms, transform 300ms;
+  transition: opacity 150ms, transform 300ms;
 
-	>div {
-		transition: transform 300ms cubic-bezier(0.22, 1, 0.36, 1);
-	}
+  > div {
+    transition: transform 300ms cubic-bezier(0.22, 1, 0.36, 1);
+  }
 }
 
 .overlay-leave-to,
 .overlay-enter-from {
-	opacity: 0;
+  opacity: 0;
 
-	>div {
-		transform: scale(.95);
-	}
+  > div {
+    transform: scale(0.95);
+  }
 }
 
 .theShadow {
-	box-shadow: 0 32px 40px 0 rgba(0, 0, 0, 0.24), inset 0 1px 1px 0 rgba(255, 255, 255, 0.25), inset 0 -1px 1px 0 rgba(255, 255, 255, 0.1);
+  box-shadow: 0 32px 40px 0 rgba(0, 0, 0, 0.24),
+    inset 0 1px 1px 0 rgba(255, 255, 255, 0.25),
+    inset 0 -1px 1px 0 rgba(255, 255, 255, 0.1);
 }
 </style>
