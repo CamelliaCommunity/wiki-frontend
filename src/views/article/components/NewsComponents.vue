8c77a7bda83fd7682bb3ce91010e311c86f4fec0
Aisuruneko <git@nekos.sh>
2024-10-14 00:56:46 -0400

    add: next and previous posts (and make random work too)

--- src/views/article/components/NewsComponents.vue
+++ src/views/article/components/NewsComponents.vue
@@ -2,78 +2,95 @@
 import { reactive } from 'vue';
 import API from '@/utils/API';
 import BlockquoteNote from '@/components/BlockquoteNote.vue';
-import FeaturedPost from '@/views/home/components/FeaturedPost.vue'; // placeholder
+import NewsPostSelector from './NewsPostSelector.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
 
 const props = defineProps({
-    type: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-        required: false
-    },
-    layout: {
-        type: String,
-        required: false
-    }
+	type: {
+		type: String,
+		required: true
+	},
+	image: {
+		type: String,
+		required: false
+	},
+	layout: {
+		type: String,
+		required: false
+	}
 });
 
 const articlePlaceholders = {
-    loading: {
-        title: 'loading...',
-        description: 'we are still fetching this for you...',
-        date: -1,
-    },
-    none: {
-        title: 'No content.',
-        description: '',
-        date: -1
-    },
-    error: {
-        title: 'Oh no!',
-        description: 'Something went wrong while loading this... :(',
-        date: -1
-    }
+	loading: {
+		title: 'loading...',
+		description: 'we are still fetching this for you...',
+		date: -1,
+	},
+	none: {
+		title: 'No content.',
+		description: '',
+		date: -1
+	},
+	error: {
+		title: 'Oh no!',
+		description: 'Something went wrong while loading this... :(',
+		date: -1
+	}
 };
 
 const react = reactive({
-    // Random
-    random: {
-        meta: articlePlaceholders.loading,
-        url: "/random"
-    }
+	// Random
+	random: {
+		meta: articlePlaceholders.loading,
+		url: "/random"
+	}
 });
 
-API.get("/articles/random").then((res) => {
-    if (res.status == 200) react.random = res.data;
-    else if (res.status == 204) react.random.meta = articlePlaceholders.none;
-    else if (res.status >= 400 || !res.status) react.random.meta = articlePlaceholders.error;
+API.get("/articles/random?type=news").then((res) => {
+	if (res.status == 200) react.random = res.data;
+	else if (res.status == 204) react.random.meta = articlePlaceholders.none;
+	else if (res.status >= 400 || !res.status) react.random.meta = articlePlaceholders.error;
 });
 
 // News
-react.news = [];
-API.get("/articles?type=news&count=3").then((res) => {
-    if (res.status == 200) react.news = res.data;
+react.nearby = { next: null, previous: null };
+API.get("/articles?type=news&count=1984").then((res) => {
+	if (res.status == 200) {
+		for (let i = 0; i < res.data.length; i++) {
+			if (res.data[i].url == route.fullPath) {
+				if (res.data[i - 1]) react.nearby.next = res.data[i - 1];
+				if (res.data[i + 1]) react.nearby.previous = res.data[i + 1];
+				break;
+			};
+		};
+	}
+	else if (res.status >= 404 || !res.status) react.nearby = { next: { meta: articlePlaceholders.error }, previous: { meta: articlePlaceholders.error } };
 });
+console.log(react.nearby[0])
+
 </script>
 
 <template>
-    <template v-if="props.type === '1'">
-        <img :src="props.image" class="object-cover" alt="Article Cover">
-    </template>
+	<template v-if="props.type === '1'">
+		<img :src="props.image" class="object-cover" alt="Article Cover">
+	</template>
 
-    <template v-else-if="props.type === '2'">
-        <!-- todo: proper random article link or something -->
-        <BlockquoteNote class="border-x-0 rounded-lg" title="Wanna Read more? Here's a random post">
-            <div class='text-3xl font-semibold mb-2'>Lorem Ipsum</div>
-            <div class="mb-2">Lorem Ipsum</div>
-            <a class="text-lg text-accent readMoreHover" target="_blank" href="https://x.com/spinnyternally">
-                Click here to read
-            </a>
-        </BlockquoteNote>
-        <!-- todo: next post and previous post (look in designs) -->
-        <FeaturedPost class="w-full" post-type="Random Post" :post="react.random" linearBackground other-image />
-        <FeaturedPost class="w-full" post-type="Random Post" :post="react.random" linearBackground other-image />
-    </template>
+	<template v-else-if="props.type === '2'">
+		<!-- todo: proper random article link or something -->
+		<BlockquoteNote class="border-x-0 rounded-lg" title="Wanna Read more? Here's a random post">
+			<div class='text-3xl font-semibold mb-2'>{{ react.random.meta.title }}</div>
+			<div class="mb-2">{{ react.random.meta.description }}</div>
+			<a class="text-lg text-accent readMoreHover" target="_blank" :href="react.random.url">
+				Click here to read
+			</a>
+		</BlockquoteNote>
+		<!-- todo: next post and previous post (look in designs) -->
+		<div>
+			<NewsPostSelector v-if="react.nearby.next" class="w-full" :post="react.nearby.next" other-image
+				displayNext=true />
+			<NewsPostSelector v-if="react.nearby.previous" class="w-full" :post="react.nearby.previous" other-image />
+		</div>
+	</template>
 </template>
